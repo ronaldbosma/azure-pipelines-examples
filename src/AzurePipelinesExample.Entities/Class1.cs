5ee89c48d36190d574350126801dc47aaeb71244
Ronald Bosma <ronald.bosma@infosupport.com>
2021-03-29 19:14:06 +0200

    Initial example solution created with `dotnet new` templates

---
+++ src/AzurePipelinesExample.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AzurePipelinesExample.Entities
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/AzurePipelinesExample.Specs/Steps/CalculatorStepDefinitions.cs
@@ -0,0 +1,39 @@
+﻿using TechTalk.SpecFlow;
+
+namespace AzurePipelinesExample.Specs.Steps
+{
+    [Binding]
+    public sealed class CalculatorStepDefinitions
+    {
+        private readonly ScenarioContext _scenarioContext;
+
+        public CalculatorStepDefinitions(ScenarioContext scenarioContext)
+        {
+            _scenarioContext = scenarioContext;
+        }
+
+        [Given("the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int number)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Given("the second number is (.*)")]
+        public void GivenTheSecondNumberIs(int number)
+        {
+            _scenarioContext.Pending();
+        }
+
+        [When("the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            _scenarioContext.Pending();
+        }
+
+        [Then("the result should be (.*)")]
+        public void ThenTheResultShouldBe(int result)
+        {
+            _scenarioContext.Pending();
+        }
+    }
+}
