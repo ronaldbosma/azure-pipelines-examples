51ec62749e7b3fe13291ed3cb79baa9a01b9fe61
Ronald Bosma <ronald.bosma@infosupport.com>
2021-07-28 08:54:35 +0200

    Add scenario for the weather forecast

---
+++ src/AzurePipelinesExample.Specs/Features/WeatherForecast.feature
@@ -0,0 +1,7 @@
+﻿Feature: Weather Forecast
+
+Example feature
+
+Scenario: Retrieve forecast
+	When I get the weather foreceast
+	Then the forecast for 5 days is returned
---
+++ src/AzurePipelinesExample.Specs/Steps/WeatherForecastStepDefinitions.cs
@@ -0,0 +1,30 @@
+﻿using AzurePipelinesExample.WebApi;
+using AzurePipelinesExample.WebApi.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using TechTalk.SpecFlow;
+
+namespace AzurePipelinesExample.Specs.Steps
+{
+    [Binding]
+    public sealed class WeatherForecastStepDefinitions
+    {
+        private readonly WeatherForecastController _weatherForecastController = new WeatherForecastController(null);
+        private IList<WeatherForecast> _result;
+
+        [When(@"I get the weather foreceast")]
+        public void WhenIGetTheWeatherForeceast()
+        {
+            _result = _weatherForecastController.Get().ToList();
+        }
+
+        [Then(@"the forecast for (.*) days is returned")]
+        public void ThenTheForecastForDaysIsReturned(int expectedNumberOfForecasts)
+        {
+            Assert.IsNotNull(_result);
+            Assert.AreEqual(expectedNumberOfForecasts, _result.Count);
+        }
+
+    }
+}
